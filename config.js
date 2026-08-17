82182486bc0353243f8d4f4131d1d67fd24d95d8
Agus Tjakra <49585004+gsdiredja@users.noreply.github.com>
2026-08-17 20:32:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxH9K3wkiZHJ4gK47q670dxa5kNXhhOaXUjWx-kyKaEJflC69yoiMCJmbRVmOnGmRFH/exec";
+
+window.SCRIPT_URL = SCRIPT_URL;
+window.API_URL = SCRIPT_URL;
+
+window.CONFIG = {
+    SCRIPT_URL: SCRIPT_URL,
+    API_URL: SCRIPT_URL
+};
